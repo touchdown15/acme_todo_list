2a1c70561ce5850b535697da720241f8c02ddef8
Carlos Santos <chvs1524@gmail.com>
2021-03-11 16:40:44 -0300

    First commit: estrutura do projeto criado

---
+++ src/main/java/com/carlossantos/acmetodolist/AcmetodolistApplication.java
@@ -0,0 +1,13 @@
+package com.carlossantos.acmetodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmetodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmetodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlossantos/acmetodolist/AcmetodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlossantos.acmetodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmetodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
