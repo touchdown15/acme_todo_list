29c40e1a01d8e6dbfd26dbbf2be2879a0d1aa69c
Carlos Santos <chvs1524@gmail.com>
2021-03-14 18:06:27 -0300

    Adicionado CrossOrigin nos Controllers

--- src/main/java/com/carlossantos/acmetodolist/controller/TaskController.java
+++ src/main/java/com/carlossantos/acmetodolist/controller/TaskController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.carlossantos.acmetodolist.entity.Task;
 import com.carlossantos.acmetodolist.service.TaskService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/task")
 public class TaskController {
--- src/main/java/com/carlossantos/acmetodolist/controller/ToDoListController.java
+++ src/main/java/com/carlossantos/acmetodolist/controller/ToDoListController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.carlossantos.acmetodolist.entity.ToDoList;
 import com.carlossantos.acmetodolist.service.ToDoListService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/todolist")
 public class ToDoListController {
