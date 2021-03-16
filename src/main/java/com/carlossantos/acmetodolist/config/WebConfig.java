c43acbf85214649f1747e55ac3bd20164bc64fbe
Carlos Santos <chvs1524@gmail.com>
2021-03-15 22:59:46 -0300

    Atualização do metodo de config CORS

--- src/main/java/com/carlossantos/acmetodolist/config/WebConfig.java
+++ src/main/java/com/carlossantos/acmetodolist/config/WebConfig.java
@@ -12,6 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*").allowedHeaders("*");
     }
 }
